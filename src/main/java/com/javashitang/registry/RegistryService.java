17899ad5edfc46ee2c20f7ec0240aff1d67e1d4b
lilimin <290059742@qq.com>
2020-09-16 13:27:55 +0800

    项目初始化

---
+++ src/main/java/com/javashitang/registry/RegistryService.java
@@ -0,0 +1,8 @@
+package com.javashitang.registry;
+
+/**
+ * @author lilimin
+ * @since 2020-09-15
+ */
+public interface RegistryService {
+}
---
+++ src/main/java/com/javashitang/remoting/NettyClientHandler.java
@@ -0,0 +1,8 @@
+package com.javashitang.remoting;
+
+/**
+ * @author lilimin
+ * @since 2020-09-15
+ */
+public class NettyClientHandler {
+}
